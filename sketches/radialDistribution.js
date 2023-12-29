078150c35649cbd72be6a6ce5e8cf063874faa53
Christine Hoang <christinehoangt@gmail.com>
2023-12-28 18:47:45 -0600

    initsave

---
+++ sketches/radialDistribution.js
@@ -0,0 +1,31 @@
+// radialDistribution.js
+let radialSketch = function(p) {
+  const numRings = 3;
+
+  p.setup = function() {
+    p.createCanvas(window.innerWidth / 2, window.innerHeight);
+    p.angleMode(p.DEGREES);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.translate(window.innerWidth / 4, window.innerHeight / 2);
+
+    for (let i = 0; i < numRings; i++) {
+      p.rotate(60);
+      drawRing();
+    }
+  };
+
+  function drawRing() {
+    for (let angle = 0; angle < 360; angle += 10) {
+      const x = p.cos(p.frameCount + angle) * (window.innerWidth / 20 + p.mouseX / 8);
+      const y = p.sin(p.frameCount / 2 + angle) * (window.innerWidth / 20 + p.mouseY / 4);
+
+      p.fill(angle);
+      p.stroke(0);
+      p.strokeWeight(2);
+      p.ellipse(x, y, 50);
+    }
+  }
+};
---
+++ sketches/redGreenScreen.js
@@ -0,0 +1,16 @@
+// redGreenScreen.js
+let redGreenSketch = function(p) {
+  p.setup = function() {
+    p.createCanvas(window.innerWidth / 2, window.innerHeight);
+    p.background('black');
+  };
+
+  p.draw = function() {
+    p.fill(p.random(255), p.random(255), p.random(0));
+    let x = p.random(p.width);
+    let y = p.random(p.height);
+    let rWidth = p.random(100);
+    let rHeight = p.random(50, 100);
+    p.rect(x, y, rWidth, rHeight, 100, 50);
+  };
+};
