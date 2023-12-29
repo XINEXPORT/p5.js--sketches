9afdd4ea92176c3549edb736eea3ae6377ccac6c
Christine Hoang <christinehoangt@gmail.com>
2023-12-28 19:25:28 -0600

    circlesplash added and hotpink rectangles

---
+++ sketches/circleSplash.js
@@ -0,0 +1,44 @@
+//circleSplash.js
+
+let circleSplashSketch = function (p) {
+    let d = 50;
+  
+    // Create a new canvas to the browser size
+    p.setup = function () {
+      p.createCanvas(p.windowWidth / 2, p.windowHeight);
+    };
+  
+    // On window resize, update the canvas size
+    p.windowResized = function () {
+      p.resizeCanvas(p.windowWidth / 2, p.windowHeight);
+    };
+  
+    // Render loop that draws shapes with p5
+    p.draw = function () {
+      p.blendMode(p.BLEND);
+      p.background("#ffa500");
+  
+      p.stroke("#8ff247");
+      p.stroke("#ee82ee");
+      p.stroke("#8ff2b5");
+      p.noFill("#8ff247");
+      p.strokeWeight(20);
+      p.strokeWeight(10);
+  
+      d += 5;
+  
+      p.blendMode(p.EXCLUSION);
+  
+      p.circle(p.width / 2, p.height / 4, d);
+      p.circle(p.width / 3, p.height / 5, d);
+      p.circle(p.width / 4, p.height / 6, d);
+  
+      p.circle(2 * d, p.height / 2, d);
+      p.circle(3 * d, p.height / 3, d);
+      p.circle(4 * d, p.height / 5, d);
+  
+      p.circle(p.width - d, p.height / 2, d);
+      p.circle(p.width - d, p.height / 4, d);
+    };
+  };
+  
---
+++ sketches/hotPinkRectangles.js
@@ -0,0 +1,38 @@
+//hotPinkReactangles.js
+let hotPinkRectanglesSketch = function(p) {
+    p.setup = function () {
+      p.createCanvas(window.innerWidth / 2, window.innerHeight);
+      p.strokeWeight(20);
+    };
+  
+    p.windowResized = function () {
+      p.resizeCanvas(windowWidth, windowHeight);
+    };
+  
+    p.draw = function () {
+      p.blendMode(p.BLEND);
+      p.background("#ff39b5");
+      p.ellipse(p.mouseX, p.mouseY, 80, 80);
+      p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+      p.frameRate(18);
+  
+      let a = p.color('green');
+      p.fill(a);
+  
+      p.rect(50, 50, 300, 100);
+      p.stroke('red');
+      p.fill('#00ccb5');
+  
+      p.rect(50, 600, 1000, 100);
+      p.stroke('black');
+      p.fill('#02ccb6');
+      p.rect(10, 300, 300, 400);
+      p.stroke('blue');
+      p.circle(500, 300, 150, 300);
+      p.stroke('orange');
+      p.line(500, 300, 150, 500);
+      p.fill("purple");
+      p.square(100, 490, 64);
+    };
+  };
+  
